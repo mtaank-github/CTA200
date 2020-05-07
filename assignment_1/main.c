4a09d460203034113c8ddbc92a5fecbbeffd5a88
Mukesh Taank <mtaank@uoguelph.ca>
2020-05-07 12:21:27 -0400

    Created a folder with everything for Assignment 1

---
+++ assignment_1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "average.h"
+
+int main() {
+    double arr[] = {1.0, 2.0, 3.0, 4.0};
+   
+    double result = average(length, arr);
+	
+    printf("The average of 1, 2, 3 and 4 is: %.4f\n", result);
+    return 0;    
+}
+
