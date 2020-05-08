a264f28f9b38f2d8de8ba4ec47a3ebf0dac34bbb
mtaank-github <64869942+mtaank-github@users.noreply.github.com>
2020-05-08 16:25:43 -0400

    Add files via upload
    Updated the codes for the Questions

---
+++ assignment_2/Q1.py
@@ -0,0 +1,75 @@
+# Mukesh Taank
+# mtaank@uoguelph.ca
+# USER: mtaank
+# CTA 200H
+# Question 1
+
+import matplotlib.pyplot as plt
+from math import sqrt as sq
+import numpy as np
+
+def plot_setup():
+    '''
+    Description:
+    Sets up the plotting parameters for the graph we want to create. Need to call plt.plot() \
+    before calling this function Sets the title, axes, x and y limits, legend and gridlines.
+    
+    Parameters:
+    No parameters
+    
+    Returns:
+    No return
+    '''
+    plt.xlim(-2,2)
+    plt.ylim(-2,2)
+    plt.xlabel('Real $(x)$')
+    plt.ylabel('Imaginary $(y)$')
+    plt.grid()
+    plt.title("Complex Plane")
+    plt.legend(loc='best')
+    plt.show()
+    return
+
+n = 10 # Number of iterations
+z0 = 0 # Initial position given z0 = 0
+
+
+# Choose points, C, of the form c = x + iy
+# Both x and y are bound by: -2 < x,y < 2
+x_vals = [-0.2, 1.8, -0.6, 1.3, 0.4]
+y_vals = [0.4, 0.6, -0.234, 1.9, -0.3]
+c_vals = [complex(x,y) for x,y in zip(x_vals, y_vals)]
+
+#Initialize some matrices
+Z = z0*np.zeros((len(c_vals),n), dtype=np.complex) #This will hold the values of z for each value of c
+reals = np.zeros((len(c_vals),n)) #This holds all the real components of z
+imags = np.zeros((len(c_vals),n)) #This holds all the imaginary components of z
+
+for i in range(len(c_vals)):
+    
+    #Print out the modulus of each number C
+    modulus_of_c = [sq(x**2+y**2) for x,y in zip(x_vals,y_vals)]
+    print("modulus of " + str(c_vals[i]), "is: ", modulus_of_c[i])
+    
+    # Now calculate the values of Z for corresponding value of C
+    for k in range(1,n):
+        Z[i][k] = Z[i][k-1]**2 + c_vals[i]
+
+        # Separate the values of Z into its real and imaginary parts for plotting
+        reals[i][k] = Z[i][k].real
+        imags[i][k] = Z[i][k].imag
+        
+    # Arrange the graphs of each on the plot
+    # If the plot diverges - make red. If the plot is contained - make blue
+    if modulus_of_c[i] > 1.0:
+        plt.plot(reals[i], imags[i], 'r-', label="c = "+str(c_vals[i]))
+    else:
+        plt.plot(reals[i], imags[i], 'b-', label="c = "+str(c_vals[i]))
+
+plot_setup()
+    
+    
+        
+    
+
+
---
+++ assignment_2/Q2.py
@@ -0,0 +1,85 @@
+# Mukesh Taank
+# mtaank@uoguelph.ca
+# USER: mtaank
+# CTA 200H
+# Question 2
+
+import numpy as np
+import scipy
+from scipy.integrate import odeint #Scipy's built-in function for solving ODEs
+import matplotlib.pyplot as plt
+
+def SIR(y, t, gamma, beta, N):
+    '''
+    Description:
+    Sets up the plotting parameters for the graph we want to create. Need to call plt.plot()
+    before calling this function.
+    
+    Parameters:
+    y - dummy variable to contain the S, I, R functions - a list
+    t - time - a list
+    gamma -  parameter representing the recovery rate
+    beta - paramter representing the infection rate - a float
+    N - Number of people in the population - an integer
+    
+    Returns:
+    This will return the differential equations needed to be solved later on and arranges \
+    them into a list.
+    '''
+    S, I, R = y #Put all our functions into one list called y
+    
+    # Now put the ODEs into a list of the form: dy/dt = [dS/dt, dI/dt, DR/dt]
+    dy_dt = [-(beta*S*I)/N, (beta*S*I)/N - gamma*I, gamma*I]
+    
+    return dy_dt
+
+def plot_setup():
+    '''
+    Description:
+    Sets up the plotting parameters for the graph we want to create. Need to call plt.plot() \
+    before calling this function Sets the title, axes, x and y limits, legend and gridlines.
+    
+    Parameters:
+    No parameters
+    
+    Returns:
+    No return
+    '''
+    plt.xlim(0,200)
+    plt.ylim(0,1000)
+    plt.xlabel('time, $t$')
+    plt.ylabel('Number of People')
+    plt.grid()
+    plt.title(r'Plot of the solution to the SIR model with $\beta$ = 1/4 and $\gamma$ = 1/3')
+    plt.legend(loc='best')
+    plt.show()
+    return
+
+
+# Now we can make choose our constants
+N = 1000
+beta = 1/10
+gamma = 1/4
+
+# Now need intital conditions for the S, I, R
+# Set up list as [S(0), I(0), R(0)]
+# We are given that:
+# S(0)=999, I(0)=1, R(0)=0
+y0 = [999., 1., 0.]
+
+# We need to integrate from t=0 to t=200
+t = np.linspace(0,200,101)
+
+# Now can use the scipy function odeint to get the solutions for S, I and R
+solutions = odeint(SIR, y0, t, args=(beta, gamma,N))
+
+# Now we can plot the solution curves
+plt.plot(t, solutions[:,0], 'r-', label='S(t) Number of People Susceptible')
+plt.plot(t, solutions[:,1], 'b-', label='I(t) Number of People Infected')
+plt.plot(t, solutions[:,2], 'g-', label='R(t) Number of People Recovered')
+plot_setup()
+
+
+
+
+
